314bb60dd4b3475c6e8f704f3cecf490a81f1f9e
BertoPolo <bertopolo91@gmail.com>
2021-12-16 15:19:49 +0100

    3

--- d4js.js
+++ d4js.js
@@ -1,2 +1,13 @@
-let people = ["greg","john","maria","bart","mathias","albert","cristian","robert","seguismundo","eufrasio","roger","mike","gerard","joseph"]
+/* let people = ["greg","john","maria","bart","mathias","albert","cristian","robert","seguismundo","eufrasio","roger","mike","gerard","joseph"]
 
+let numGroups
+
+let groups 
+
+for(let i=0;i<people.length;i++){
+    console.log(people[i])
+}
+ */
+
+
+const 
