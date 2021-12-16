7797079c0a0a8a355f9d9feced9ee5cd406192ca
BertoPolo <bertopolo91@gmail.com>
2021-12-16 16:29:11 +0100

    Update d4js.js

--- d4js.js
+++ d4js.js
@@ -13,7 +13,7 @@ for(let i=0;i<people.length;i++){
 
 const waitingPersonsLi = document.getElementById("waitingPersonsLi")
 
-//create a person and attach to a waiting list
+//create a person and attach to a waiting list --WORKING--
 const personCreator = function(){
     const personAdder = document.getElementById("personAdder")
     const li = document.createElement("li")
@@ -21,11 +21,21 @@ const personCreator = function(){
     waitingPersonsLi.appendChild(li)
 
 }
-//
 
 
-
-//divide on groups
+//divide on groups NOT FINISHED
 const numDivider = function(){
     const numberGroups = document.getElementById("numberGroups")
+    let divider = waitingPersonsLi / numberGroups
+    
 }
+
+
+
+//assign a variable  with math floor random  in
+//use that variable on person-index to grab it
+//how to use the index? where is the output of personCreator???
+
+
+let randomIndex = Math.floor(Math.random())* waitingPersonsLi.length // with this you have a random number
+
